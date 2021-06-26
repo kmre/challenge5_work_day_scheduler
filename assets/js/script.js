b56b75db4e98cf0c57c243da33a4dcae2685e316
kmre <karla.romero7@gmail.com>
2021-06-25 20:30:25 -0400

    skeleton for times completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,131 +1,57 @@
 var tasks = {};
+var segments = [];
+var segmentByHour = [];
 
-/*var createTask = function(taskText, taskDate, taskList) {
-    // create elements that make up a task item
-    var taskLi = $("<li>").addClass("list-group-item");
-    var taskSpan = $("<span>")
-      .addClass("badge badge-primary badge-pill")
-      .text(taskDate);
-    var taskP = $("<p>")
-      .addClass("")
-      .text(taskText);
-  
-    // append span and p element to parent li
-    taskLi.append(taskSpan, taskP);
-  
-    // check due date
-    //auditTask(taskLi);
-  
-    // append to ul list on the page
-    //$("#list-" + taskList).append(taskLi);
-  };*/
-  /*
-  var loadTasks = function() {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-  
-    // if nothing in localStorage, create a new object to track time
-    if (!tasks) {
-      tasks = {
-        time: []
-      };
+function setToday(params) {
+    var today = moment().format('MMMM Do YYYY, h:mm:ss a'); 
+    console.log(today)
+    $("#currentDay").append(today);
+}
+
+setToday();
+
+createDivs();
+
+//create divs for every hr of the day and run other fn
+function createDivs() {
+    new Array(24).fill().forEach((value, index) => {
+        segmentByHour.push(moment( {hour: index} ).format('h:mm A'));
+    })
+
+    for (let index = 0; index <= 23; index++) {
+        debugger;
+        createRowsFn(index);
+        hrDivFn(index);
+        hrPFn(index);
     }
-  
-    // loop over object properties
-    $.each(tasks, function(list, arr) {
-      // then loop over sub-array
-      arr.forEach(function(task) {
-        createTask(task.text, task.date, list);
-      });
-    });
-  };
-  */
-  var saveTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  };
-  
-  var auditTask = function(taskEl) {
-    // get date from task element
-    var date = $(taskEl)
-      .find("span")
-      .text()
-      .trim();
-  
-    // convert to moment object at 5:00pm
-    var time = moment(date, "L").set("hour", 17);
-  
-    // remove any old classes from element
-    $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
-  
-    // apply new class if task is near/over due date
-    if (moment().isAfter(time)) {
-      $(taskEl).addClass("list-group-item-danger");
-    } else if (Math.abs(moment().diff(time, "days")) <= 2) {
-      $(taskEl).addClass("list-group-item-warning");
-    }
-  };
-  
-//////////////////////////////////////////////////////////////////////////////////////
+}
 
-// task segment was clicked
-$(".seg-input").on("click", "p", function() {
-    // get current text of p element
-    var text = $(this)
-      .text()
-      .trim();
-  
-    // replace p element with a new textarea
-    var textInput = $("<textarea>").addClass("form-control").val(text);
-    $(this).replaceWith(textInput);
-  
-    // auto focus new element
-    textInput.trigger("focus");
-    console.log(textInput)
-  });
-  
-  
-  // editable field was un-focused
-  $(".seg-input").on("blur", "textarea", function() {
-    // get current value of textarea
-    var text = $(this).val();
-    console.log(text)
-    /*
-    // get status type and position in the list
-    
-    var status = $(this)
-      .closest(".seg-input")
-      .attr("id")
-    var index = $(this)
-      .closest(".seg-input")
-      .index();
-    
-  */
-    // update task in array and re-save to localstorage
-    
-    tasks.text = text;
-    
-    
-    saveTasks();
-  
-    //recreate p element
-    var taskP = $("<p>")
-      .addClass("")
-      .text(text);
-  
-    // replace textarea with new content
-    $(this).replaceWith(taskP);
-    console.log(taskP)
-   
-  });
+//create div with row class for each index
+function createRowsFn(index) {
 
-  // load tasks for the first time
-//loadTasks();
+    var divCreateRow = $("<div>").addClass("hr-seg row i" + index)
+    $("#main-container").append(divCreateRow)
+    $(divCreateRow).attr("id", "independent-rows" + index)
+    debugger;
+    
+}
 
-// audit task due dates every x min
-var min = 30;
+//add the hr div to the row class
+function hrDivFn(index) {
+    var divCreateHr = $("<div>").addClass("seg-time col-4 d-flex justify-content-center align-items-center i" + index)
+    $("#independent-rows" + index).append(divCreateHr)
+    $(divCreateHr).attr("id", "seg-time" + index)
+    debugger;
+}
 
-setInterval(function() {
-  $(".card .list-group-item").each(function() {
-    auditTask($(this));
-  });
-}, (60*1000)*min);
-  
+//add the hr time to each hr div
+function hrPFn(index) {
+    var hrP = $("<p>").addClass("time-by-hr i" + index)
+        .text(segmentByHour[index])
+        console.log(hrP)
+        console.log(segmentByHour[index])
+        $("#seg-time" + index).append(hrP)
+        $(hrP).attr("id", "time-by-hr" + index)
+}
+
+
